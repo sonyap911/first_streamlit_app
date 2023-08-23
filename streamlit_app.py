54943bf4ee2cf52c8d7cf3df648f0124901588b4
sonyap911 <72713616+sonyap911@users.noreply.github.com>
2023-08-22 22:33:14 -0600

    Update streamlit_app.py
    Try/except

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,16 +25,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-#New Section to display fruityvice api response streamlit.header ('Fruityvice Fruit Advice!')
-streamlit.header("Fruityvice Fruit Advice!")
-
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize json response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# normalize table 
-streamlit.dataframe(fruityvice_normalized)
+#New Section to display fruityvice api response streamlit. header ('Fruityvice Fruit Advice!')
+try:
+fruit_choice = streamlit.text_input ('What fruit would you like information about?')
+if not fruit_choice:
+streamlit.error"Please select a fruit to get information.
+")
+else:
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit choice)
+fruityvice_normalized = pandas. json_normalize(fruityvice_response.json ())
+streamlit.dataframe (fruityvice normalized)
+except URLError as e:
+streamlit.error ()
 # import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
