b903d001b4a957e8b37b4118a8d870fb384f8796
sonyap911 <72713616+sonyap911@users.noreply.github.com>
2023-08-22 22:04:26 -0600

    Update streamlit_app.py
    Forgot comma

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,5 @@ streamlit.dataframe(my_data_rows)
 
 # allow end user to add another fruit
 add_my_fruit= streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding' add_my_fruit)
+streamlit.write('Thanks for adding', add_my_fruit)
 
